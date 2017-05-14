41bb196b0f78ecca06f520985d1d737d2893c7d8
Kakise <me@kakise.xyz>
2017-05-14 15:40:11 +0200

    Fixed ordering

--- services/tags.js
+++ services/tags.js
@@ -4,6 +4,7 @@ const client = require('./contentfulClient.js').client;
 function listTags(tag) {
 	query = {
 		content_type: 'blogPost',
+		order: '-fields.date',
 		'fields.tags': tag
 	};
 	return client.getEntries(query)
