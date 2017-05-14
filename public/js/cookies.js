f63213edea61137b09693ac51e626ef40f3a558d
Labo Member 004 <me@kakise.xyz>
2017-05-14 14:45:18 +0200

    Create cookies.js

---
+++ public/js/cookies.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#1d8a8a"
+    },
+    "button": {
+      "background": "transparent",
+      "text": "#62ffaa",
+      "border": "#62ffaa"
+    }
+  },
+  "content": {
+    "message": "Ce site web utilise des cookies afin d'assurer son bon fonctionnement. En le visitant, vous acceptez l'enregistrement de cookies sur votre machine.",
+    "dismiss": "Compris!",
+    "link": "En savoir plus",
+    "href": "/page/cookies"
+  }
+})});
